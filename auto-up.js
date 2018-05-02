18c9e64c0269f7d2025684eeb8fc7da16212c61f
qiuwanlin <35997491+qiuwanlin@users.noreply.github.com>
2018-05-02 22:57:16 +0800

    Add files via upload

---
+++ auto-up.js
@@ -0,0 +1,48 @@
+let special = document.querySelectorAll('[data-x]')
+  for (let i = 0; i < special.length; i++) {
+    special[i].classList.add('offset')
+  }
+  setTimeout(function () {
+    findClosest()
+  }, 1000);
+  window.addEventListener('scroll', function (qqq) {
+    fingClosest()})
+   window.addEventListener('scroll',function(x){ 
+    if (window.scrollY > 0) {
+      topbar.classList.add('sticky')
+    } else {
+      topbar.classList.remove('sticky')
+    }
+    findClosest()
+  })
+
+  function findClosest() {
+    let special = document.querySelectorAll('[data-x]')
+    let min = 0
+    for (let i = 1; i < special.length; i++) {
+      if (Math.abs(special[i].offsetTop - window.scrollY) < Math.abs(special[min].offsetTop - window.scrollY)) { min = i }
+    }
+    special[min].classList.remove('offset')
+
+    let id = special[min].id
+    let a = document.querySelector('a[href="#' + id + '"]')
+    let li = a.parentNode
+    let family = li.parentNode.children
+    for (let i = 0; i < family.length; i++) {
+      family[i].classList.remove('light')
+    }
+    li.classList.add('light')
+  }
+
+
+
+
+  let liTags = document.querySelectorAll('nav.menu > ul > li')
+  for (let i = 0; i < liTags.length; i++) {
+    liTags[i].onmouseenter = function (x) {
+      x.currentTarget.classList.add('active')
+    }
+    liTags[i].onmouseleave = function (x) {
+      x.currentTarget.classList.remove('active')
+    }
+  }
